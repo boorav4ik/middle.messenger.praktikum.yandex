3f04680951ab9331518a2d427ccfa6de4ab7e7e3
maxonline <maxonline@burut.org>
2022-11-15 12:09:01 +0300

    feat(server): format log

--- server.js
+++ server.js
@@ -5,5 +5,8 @@ const PORT = 3000;
 
 app.use(express.static("./dist"));
 app.listen(PORT, function () {
-  console.log(`Example app listening on port ${PORT}! Dirname ${__dirname}`);
+  console.log(`
+    Example app listening on port ${PORT}!
+    Dirname ${__dirname}
+  `);
 });
