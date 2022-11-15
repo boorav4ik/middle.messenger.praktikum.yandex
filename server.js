b41eeb5e76c9a682585f23058a8786b7819016fc
Max <maxonline@mail.ru>
2022-11-16 01:11:43 +0300

    feat(404, 500): add error pages

--- server.js
+++ server.js
@@ -2,8 +2,10 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+const STATIC = "./dist"
 
-app.use(express.static("./dist"));
+app.use(express.static(STATIC));
+
 app.listen(PORT, function () {
   console.log(`
     Example app listening on port ${PORT}!
