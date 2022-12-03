fd5c1da2d18a97ca0eb5fc00346aaa47eebee749
Max Trufanov <maxonline@mail.ru>
2022-12-03 10:02:03 +0300

    fix(Navbar): add Navlink

--- src/components/Navbar/index.ts
+++ src/components/Navbar/index.ts
@@ -1,4 +1,5 @@
 import Block from "../../utils/Block";
+import NavLink from "../NavLink";
 import template from "./navbar.hbs";
 import "./index.css";
 
@@ -12,6 +13,13 @@ export default class Navbar extends Block {
     super("nav", props);
   }
 
+  init() {
+    this.children.logo = new NavLink({ label: this.props.logo, to: "/" });
+    Object.entries(this.props.pages).forEach(
+      ([label, to]) =>
+        (this.children[label] = new NavLink({ label, to: String(to) }))
+    );
+  }
   render() {
     return this.compile(template, this.props);
   }
