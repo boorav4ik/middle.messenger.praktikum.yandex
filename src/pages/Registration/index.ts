13395393042f8b0ecedab77e30bc62ff633f9d6a
Max Trufanov <maxonline@mail.ru>
2023-01-16 20:15:23 +0300

    add helpers, connection form and controllers

--- src/pages/Registration/index.ts
+++ src/pages/Registration/index.ts
@@ -1,16 +1,18 @@
 import Block from "../../utils/Block";
 import fields from "../../mock/registrationFieldList";
+import AuthController from "../../controllers/AuthController";
 
 interface IRegistrationPageProps {
   fields: Record<string, unknown>;
-  onSubmit: () => void;
+  onSubmit: (data: Record<string, string>) => void;
 }
 export default class RegistrationPage extends Block<IRegistrationPageProps> {
   constructor() {
+    document.title = "Chokak - SingUp";
     super({
       fields,
-      onSubmit() {
-        location.replace("./messenger");
+      onSubmit(data: Record<string, string>) {
+        AuthController.signup(data);
       }
     });
   }
